2d316cd1bb5bda06e324dcf452e5790059b50070
hasithan <hasithan@hsenidmobile.com>
2019-11-28 14:29:37 +0530

    refactered visiter counter

--- src/main/java/com/hsenid/v1/VisitorCount.java
+++ src/main/java/com/hsenid/v1/VisitorCount.java
@@ -1,10 +1,23 @@
 package com.hsenid.v1;
 
-public class VisitorCount {
-    public void incrementCount() {
+import java.util.concurrent.atomic.AtomicInteger;
 
+public class VisitorCount {
+
+    private static VisitorCount instance = null;
+    private AtomicInteger visitorCount = new AtomicInteger(0);
+
+    private VisitorCount() {
     }
-    public static VisitorCount getInstance() {
-        return new VisitorCount();
+
+    public static synchronized VisitorCount getInstance() {
+        if (instance == null) {
+            instance = new VisitorCount();
+        }
+        return instance;
+    }
+
+    public void incrementCount() {
+        visitorCount.incrementAndGet();
     }
 }
--- src/main/java/com/hsenid/v1/cf/Session.java
+++
@@ -1,4 +0,0 @@
-package com.hsenid.v1.cf;
-
-class Session {
-}
